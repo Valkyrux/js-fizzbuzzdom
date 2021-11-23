5c3d8d29a03801b9c1591e1a0d397f092dd7a831
Valkyrux <philadephos.granata@gmail.com>
2021-11-23 16:12:08 +0100

    esercizio-completo

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// seleziono la ul alla quale aggiungere gli li
+const ul = document.querySelector(".container ul");
+
+for (let i = 0; i < 100; i++) {
+    // creo l'elemento da aggiungere all'index
+    const li = document.createElement("li");
+    const innerText = li.innerHTML = i + 1;
+    if((innerText % 15) == 0) {
+        li.classList = "red";
+        li.innerHTML = "fizzbuzz";
+    } else if ((innerText % 3) ==0) {
+        li.classList = "green";
+        li.innerHTML = "fizz";
+    } else if ((innerText % 5) ==0) {
+        li.classList = "yellow";
+        li.innerHTML = "buzz";
+    }
+    ul.append(li);
+}
